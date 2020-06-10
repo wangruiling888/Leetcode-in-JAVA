9368282dad1818f017f01cf8f4fb5dbe81166733
Ruiling Wang <43671776+wangruiling888@users.noreply.github.com>
2020-06-11 00:40:49 +1000

    Update Two Sum.java

--- Two Sum.java
+++ Two Sum.java
@@ -1,3 +1,4 @@
+// method 1:
 // Brute Force: traverse each combination one by one and check if there's sum that equals to target
 // BigO: O(n^2)
 class Solution {
@@ -10,6 +11,28 @@ class Solution {
                     result[1] = j;
                 }
             }
+        }
+        return result;
+    }
+}
+// method 2:
+// Hashmap: average time complexity of insertion and search in hashMap are both O(1)
+// BigO: O(n) cuz we just traverse nums once
+// put: insert key-value pair into map
+// get: get value by key from map
+// remove: remove key-value pair from map and return value
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numsMap = new HashMap<Integer, Integer>();
+        int result[] = new int[2];
+        for (int i = 0; i < nums.length; i++){
+            int differ = target - nums[i];
+            if (numsMap.containsKey(differ)){
+                result[0] = numsMap.get(differ);
+                result[1] = i;
+                return result;
+            }
+            numsMap.put(nums[i], i);
         }
         return result;
     }
